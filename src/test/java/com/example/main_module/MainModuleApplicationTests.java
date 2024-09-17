7cebc879204b3c095501974a4ee45292a99f4711
bukwon <dnu05043@gmail.com>
2024-09-17 14:40:17 +0900

    module-test

---
+++ src/test/java/com/example/main_module/MainModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.main_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
